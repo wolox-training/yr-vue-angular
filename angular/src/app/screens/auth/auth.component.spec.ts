dd1984f1610aee5654a206cf8ba60337f86c9f3e
Yonathan Ramos <yonathan.g.ramos@accenture.com>
2022-08-08 15:42:28 -0500

    set auth component tests

--- angular/src/app/screens/auth/auth.component.spec.ts
+++ angular/src/app/screens/auth/auth.component.spec.ts
@@ -1,22 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { render } from '@testing-library/angular';
-import { screen } from '@testing-library/dom';
+import { fireEvent, screen } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 
 describe('Render auth component', () => {
+  let routerSpy = { navigate: jest.fn() };
   beforeEach(async () => {
     render(AuthComponent, {
       declarations: [AuthComponent, NavbarComponent],
       imports: [CommonModule, AuthRoutingModule, HttpClientModule],
+      providers: [{ provide: Router, useValue: routerSpy }],
     });
   });
+
   it('logout button exists', async () => {
     const btnLogout = screen.getByText('Logout');
+    fireEvent.click(btnLogout);
 
     expect(btnLogout).toBeTruthy();
+  });
+
+  it('verify session closure with the logout button', async () => {
+    const btnLogout = screen.getByText('Logout');
+    fireEvent.click(btnLogout);
+
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
